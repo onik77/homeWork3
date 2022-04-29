f13cf686a8d247389e82d83ae561c208f2760191
Onik <nick.grigoryan98@gmail.com>
2022-04-29 20:03:45 +0400

    new commit

--- homeWork3/main.swift
+++ homeWork3/main.swift
@@ -7,5 +7,93 @@
 
 import Foundation
 
-print("Hello, World!")
+//HomeWork 3_1
+
+//հայտարարել հաստատւներ a = 12,  b = -34.55, hiText = “Hello”, itIsBool = true,
+//նշված հաստատւները խմբաորել Tuple-ի միջոցով  all հաստատունում:
+
+//let a = 12
+//let b = -34.55
+//let hiText = "Hello"
+//let itlsBool = true
+//
+//let all = (a , b , hiText , itlsBool)
+
+//homeWork3.2հայտարարել հաստատւներ a = 12,  b = -34.55, hiText = “Hello”, itIsBool = true,
+//նշված հաստատւները խմբաորել Tuple-ի միջոցով  all փոփոխականում ,
+//փոխել all փոփոխականում (Tuples) գտնող b-ին համապատասխանող արժեքը 12.5125:
+
+
+//let a = 12
+//let b = -34.55
+//let hiText = "Hello"
+//let itlsBool = true
+//
+//var all = (a , b , hiText , itlsBool)
+//var tuple2 = all.1
+//tuple2 = 12.5125
+//print(tuple2)
+
+
+//homeWork3.3հայտարարել հաստատւներ a = 12,  b = -34.55, hiText = “Hello”, itIsBool = true
+//նշված հաստատւները խմբաորել Tuple-ի միջոցով  all փոփոխականում,
+//all փոփոխականում (Tuples) գտնող a-ին համապատասխանող արժեքը մեծազնել 2 անգամ:
+
+//let a = 12
+//let b = -34.55
+//let hiText = "Hello"
+//let itlsBool = true
+//let all = (a , b , hiText , itlsBool)
+//var tuple1 = all.0 * 2
+//print(tuple1)
+
+//homeWork3.4հայտարարել հաստատւներ a = 12.55,  b = -34.55, hiText = “Hello”, itIsBool = true ,
+//նշված հաստատւները խմբաորել Tuple-ի միջոցով  all փոփոխականում,
+//all փոփոխականում (Tuples) գտնող a-ին համապատասխանող արժեքին գումարել  b-ին համապատասխանող արժեքը:
+////let a:Float = 12
+//let b:Float = -34.55
+//let hiText = "Hello"
+//let itlsBool = true
+//let all = (a , b , hiText , itlsBool)
+//
+//var tuple2 = all.0 + all.1
+//print(tuple2)
+
+
+//homework3.5հայտարարել Tuple հաստատւն,  որը կխմբաորի հետևյալ արժեքները իրենց համապատասխան անուներով  a։ 12.55,  b։ -34.55, hiText։ “Hello”, itIsBool։ true
+
+
+//let tuples = (a:12.55 , b:-34.55 , hiText:"Hello" , isitBool: true)
+//
+//print(tuples)
+
+
+//homeWork3.7հայտարարել Optional հաստատւներ a = 12,  b = -34.55, hiText = “Hello”, itIsBool = true,
+
+//let a:Int? = 12
+//let b:Double? = -34.55
+//let hiText:String? = "Hello"
+//let itlsBool:Bool? = true
+
+//homeWork3.8հայտարարել Optional հաստատւներ a = 12,  b = -34.55, hiText = “Hello”, itIsBool = true,
+//տերնարնի և ?? օպերատոռների միջոցով վերցնել արժեքները և վերագրել a_ b_ hiText_ itIsBool_  հաստատւներին իսկ եթե Optional-ի արժեքը nil է ապա համապտասղանաբառ վերագրել 0, -1, “nil”, false
+//
+
+//let a:Int? = 12
+//let b:Double? = -34.55
+//let hiText:String? = "Hello"
+//let itlsBool:Bool? = true
+
+//let a1: Int = a ?? 0
+//let b1: Double = b ?? -1
+//let hiText1 = hiText == nil ? "nil" : hiText!
+//let itlsBool1: Bool = itlsBool ?? false
+//print(a1)
+//print(b1)
+//print(hiText1)
+//print(itlsBool1)
+//
+//
+
+
 
